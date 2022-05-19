5ca9d43cce35103ca91ae159e74f990ca043f23a
antonprihodko <antonprihodko@192.168.4.198>
2022-05-19 19:52:54 +0400

    Add sql file and properties for DB

---
+++ sql/src/main/resources/sql/createCourses.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS school.courses
+(
+   course_id bigserial NOT NULL,
+   course_name text COLLATE pg_catalog."default" NOT NULL,
+   course_description text COLLATE pg_catalog."default" NOT NULL
+)
+
