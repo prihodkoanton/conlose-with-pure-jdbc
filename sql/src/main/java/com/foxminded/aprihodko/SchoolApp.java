44ddfb4eee4f06d5b26b5ff239f468f0d85ec809
antonprihodko <antonprihodko@localhost>
2022-05-21 13:47:46 +0400

    Change main method

--- sql/src/main/java/com/foxminded/aprihodko/SchoolApp.java
+++ sql/src/main/java/com/foxminded/aprihodko/SchoolApp.java
@@ -8,9 +8,11 @@ import java.util.Properties;
 import com.foxminded.aprihodko.dao.datasource.Datasource;
 import com.foxminded.aprihodko.dao.datasource.SimpleDatasorce;
 import com.foxminded.aprihodko.dao.impl.CourseDaoImpl;
+import com.foxminded.aprihodko.dao.impl.GroupDaoImpl;
 import com.foxminded.aprihodko.dao.impl.StudentsDaoImpl;
 import com.foxminded.aprihodko.misc.GenerateCourses;
 import com.foxminded.aprihodko.misc.GenerateStudents;
+import com.foxminded.aprihodko.misc.GeneratorGroups;
 import com.foxminded.aprihodko.utils.SqlUtils;
 
 import static com.foxminded.aprihodko.utils.TransactionUtils.transaction;
@@ -20,21 +22,30 @@ public class SchoolApp implements Closeable {
 
    private final Datasource datasource;
 //   private final StudentsDaoImpl studentsDao;
-   private final CourseDaoImpl courseDao;
+//   private final GroupDaoImpl groupDao;
+   private final CourseDaoImpl courseDao; // Course works
 
    public SchoolApp(Datasource datasource) throws SQLException {
       this.datasource = datasource;
-      SqlUtils.executeSqlScriptFile(datasource, "sql/createCourses.sql");
       this.courseDao = new CourseDaoImpl();
+      SqlUtils.executeSqlScriptFile(datasource, "sql/createCourses.sql");
+//      this.studentsDao = new StudentsDaoImpl();
+//      SqlUtils.executeSqlScriptFile(datasource, "sql/createStudents.sql");
+//      this.groupDao = new GroupDaoImpl();
+//      SqlUtils.executeSqlScriptFile(datasource, "sql/createGroups.sql");
    }
 
    private void run() throws SQLException {
+//      transaction(datasource, (connection -> new GeneratorGroups(groupDao).generateDate(connection, 10)));
+//      transaction(datasource, (connection -> new GenerateStudents(studentsDao).generateData(connection, 200)));
       transaction(datasource, (connection -> new GenerateCourses(courseDao).generateDate(connection, 10)));
    }
 
    @Override
    public void close() throws IOException {
       try {
+//         SqlUtils.executeSqlScriptFile(datasource, "sql/createStudents.sql");
+//         SqlUtils.executeSqlScriptFile(datasource, "sql/createGroups.sql");
          SqlUtils.executeSqlScriptFile(datasource, "sql/createCourses.sql");
       } catch (SQLException e) {
          throw new IOException(e);
@@ -48,5 +59,4 @@ public class SchoolApp implements Closeable {
          schoolApp.run();
       }
    }
-
 }
