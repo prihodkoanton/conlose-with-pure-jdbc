ae545792df622a5727bb5f11616f34c155278b62
Anton Prihodko <tohathebest97@gmail.com>
2022-05-24 20:27:54 +0000

    Delete find_all_courses.sql

--- sql/src/test/resources/sql/find_all_courses.sql
+++
@@ -1,5 +0,0 @@
-insert into school.courses (course_id, course_name, course_description)
-values (1000, 'one', 'one description'),
-       (1001, 'two', 'two description'),
-       (1002, 'three', 'three description'),
-       (1003, 'four', 'four description');
